55a78724398137286b4655184d093a819de5d81a
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2018-12-09 14:48:59 +0530

    Update url_validation_regular_expression.js

--- url_validation_regular_expression.js
+++ url_validation_regular_expression.js
@@ -1,5 +1,5 @@
-/* Regex for valid url (not include ip address) */
-/* output will give an array on match and value at index are below
+/* Regex for valid url (not include ip address)
+* output will give an array on match and value at index are below
 * 0 - Searched url
 * 3 - header (http,https,ftp)if any
 * 4 -hostname
@@ -9,15 +9,31 @@ reg0 = /^(((http|https|ftp):\/+){0,1})((([a-zA-Z0-9][a-zA-Z0-9_-]*[.])+[a-zA-Z]+
 
 //------------------------------------------------------------------------------------------------------------
 
-/* Regex for valid ip as url in ip v4*/
-/* included all ip classes (private | public also included) */
+/* Regex for valid ip as url in ip v4
+* included all ip classes (private | public also included) 
+*/
 
 reg1 = /^(((http|https|ftp):\/+){0,1})(([0-1]([0-9]{0,1})([0-9]{0,1})[.])|([2][0-5][0-5][.])|([3-9][0-9][.]))(([0-1]([0-9]{0,1})([0-9]{0,1})[.])|([2][0-5][0-5][.])|([3-9][0-9][.]))(([0-1]([0-9]{0,1})([0-9]{0,1})[.])|([2][0-5][0-5][.])|([3-9][0-9][.]))(([0-1]([0-9]{0,1})([0-9]{0,1}))|([2][0-5][0-5])|([3-9][0-9])):((([1][0][2][4-9])|([1][0][3-9][0-9])|([1][1-9][0-9][0-9])|([2-9][0-9][0-9][0-9])|([1-5][0-9][0-9][0-9][0-9])|([6][1-4][0-9][0-9][0-9])|([6][5][0-4][0-9][0-9])|([6][5][5][0-2][0-9])|([6][5][5][3][0-5]))$)/;
 
 
 //------------------------------------------------------------------------------------------------------------
 
-/* Regex for valid url included ip v4*/
-/* included all ip classes (private | public also included) */
+/* Regex for valid url included ip v4
+* included all ip classes (private | public also included)
+*/
 
 reg2 = /^(((http|https|ftp):\/+){0,1})(((((([0-1]([0-9]{0,1})([0-9]{0,1})[.])|([2][0-5][0-5][.])|([3-9][0-9][.]))(([0-1]([0-9]{0,1})([0-9]{0,1})[.])|([2][0-5][0-5][.])|([3-9][0-9][.]))(([0-1]([0-9]{0,1})([0-9]{0,1})[.])|([2][0-5][0-5][.])|([3-9][0-9][.]))(([0-1]([0-9]{0,1})([0-9]{0,1}))|([2][0-5][0-5])|([3-9][0-9])))|localhost):((([1][0][2][4-9])|([1][0][3-9][0-9])|([1][1-9][0-9][0-9])|([2-9][0-9][0-9][0-9])|([1-5][0-9][0-9][0-9][0-9])|([6][1-4][0-9][0-9][0-9])|([6][5][0-4][0-9][0-9])|([6][5][5][0-2][0-9])|([6][5][5][3][0-5]))$))|((([a-zA-Z0-9][a-zA-Z0-9_-]*[.])+[a-zA-Z]+))+([/]?[a-zA-Z0-9_?=.]+)*)/;
+
+
+//------------------------------------------------------------------------------------------------------------
+
+/* Simple regex that can return url without any header or arguments 
+* for example if any url like http://www.abc.bcd.efg.com/root/boot/folder/link and then this reg can
+* give you value 'abc.bcd.efg.com' at index of 4.
+* also work with no headers i.e www.abc.bcd.com --> abc.bcd.efg.com
+*/
+
+reg3 = /(http:|https:|ftp:)?[/]+((www+)[\.])?([a-z0-9\._-]*)/
+
+
+//------------------------------------------------------------------------------------------------------------
